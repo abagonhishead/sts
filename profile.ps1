df11f9ec591a233dc7543521c2c526c847754a9a
Russell Webster <russ@rwebster.co.uk>
2017-07-28 18:16:31 +0100

    Delete profile.ps1

--- profile.ps1
+++
@@ -1,8 +0,0 @@
-﻿<####WEBSTR4
-        Russell Webster <russ@rwebster.co.uk> <russell.webster@aviva.com>
-        Russell's global profile - Personal
-#>
-
-
-Set-alias l ls
-cd "D:\scripts\powershell"
